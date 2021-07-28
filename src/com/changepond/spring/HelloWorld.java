09200e7777e612eb6c18a0aac0a2f268f7ffddf6
pradeep s <pradeepselventhiran01@gmail.com>
2021-07-28 18:39:46 +0530

    Your message about the commit

---
+++ src/com/changepond/spring/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.changepond.spring;
+
+public class HelloWorld {
+	private String message;
+
+	public void getMessage() {
+	 System.out.println(message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
